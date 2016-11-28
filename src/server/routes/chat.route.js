4ab7087d7b938eca1a3505cf8b7a744c9cba2e76
Yangwoo Lee <lyw0149@gmail.com>
2016-11-28 20:20:17 +0900

    sendbird callback test

---
+++ src/server/routes/chat.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import jwtUtil from '../utils/jwt.util';
+import * as user from '../controllers/users.controller';
+
+/*
+ * Define the URL routing for http://yoda-domain.com/users/*
+ */
+
+const router = express.Router();
+
+
+router.post('/callback', (req,res,next) => {
+  console.log(req.body);
+});
+
+
+export default router;

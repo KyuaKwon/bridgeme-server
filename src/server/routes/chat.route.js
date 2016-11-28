98990849a0c51c91914be4f61f67f9cf06e0fe37
Yangwoo Lee <lyw0149@gmail.com>
2016-11-28 20:58:30 +0900

    sendbird callback test

--- src/server/routes/chat.route.js
+++ src/server/routes/chat.route.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post('/callback', (req, res, next) => {
   console.log(req.body);
-  res.statusCode(200).json();
+  res.status(200).json();
 });
 
 export default router;
